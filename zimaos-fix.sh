27a565a4401087b77c9288bbd5ae5efb0640cf44
老竭力 <jerrykuku@qq.com>
2024-07-08 15:14:56 +0800

    Update zimaos-fix.sh

--- zimaos-fix.sh
+++ zimaos-fix.sh
@@ -2,16 +2,23 @@
 
 counter=0
 temp_file=$(mktemp)
+tmpRoot="/tmp/root"
+mkdir -p "$tmpRoot"
+rootfs=$(mount | grep ' / ' | cut -d ' ' -f 1)
+mount "$rootfs" /tmp/root > /dev/null 2>&1
 
 find /mnt/overlay/etc/ -type f -exec sh -c 'for f; do [ ! -s "$f" ] && echo "$f"; done' sh {} + > "$temp_file"
 
 while IFS= read -r file; do
     rm "$file"
+    new_file=$(echo "$file" | sed 's/\/mnt\/overlay/\/tmp\/root/')
+    cp "$new_file" "$file"
     counter=$((counter + 1))
     echo "Fixed file: $file"
 done < "$temp_file"
 
 rm "$temp_file"
+umount "$tmpRoot"
 
 if [ $counter -gt 0 ]; then
     echo "Done! Please reboot your device."
